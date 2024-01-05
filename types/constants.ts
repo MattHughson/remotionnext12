1905a59f0893721142f945417a408daac752e497
MattHughson <matthew.hughson@lesappelsillimites.com>
2024-01-05 14:24:02 -0500

    fixing the height issue

--- types/constants.ts
+++ types/constants.ts
@@ -15,5 +15,5 @@ export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
 
 export const DURATION_IN_FRAMES = 200;
 export const VIDEO_WIDTH = 1280;
-export const VIDEO_HEIGHT = 720;
+export const VIDEO_HEIGHT = 1000;
 export const VIDEO_FPS = 30;
